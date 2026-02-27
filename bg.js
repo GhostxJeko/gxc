3a0d6fa5a1128aca12eeae43c0f7fe230f8c8b9b
GhostxJeko <jeffthein111@gmail.com>
2026-02-27 05:31:41 +0100

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,4 +1,3 @@
-
 // bg.js – Schwarzes/blaues Universum mit weißen/blauen Partikeln, Lichtball, Explosion & Strudel
 const canvas = document.getElementById("bgCanvas");
 const ctx = canvas.getContext("2d");
