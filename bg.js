52561fbceaa99b607a08634891b58a9d93d78747
GhostxJeko <jeffthein111@gmail.com>
2026-02-27 01:06:18 +0100

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
 // bg.js – High-End Partikel-Tunnel mit Strudel-Explosion
 const canvas = document.getElementById('bgCanvas');
 const ctx = canvas.getContext('2d');
@@ -135,3 +136,4 @@ window.addEventListener('resize', () => {
 
 // START
 animate();
+  });
